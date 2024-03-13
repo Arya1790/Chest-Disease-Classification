971bdf98f0585cbde43128c6215ecd66affa7cc6
Arya <arya1790@gmail.com>
2024-03-13 09:28:30 +0530

    data ingestion component added and working

---
+++ main.py
@@ -0,0 +1,13 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraningPipeline
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTraningPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\n=================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
